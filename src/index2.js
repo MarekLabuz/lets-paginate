d06d026f84f6b9e65eab27c5a5ecc5732689a512
MarekLabuz <mrk.labuz@gmail.com>
2017-01-25 13:19:36 +0100

    mergeKeys

--- src/index2.js
+++ src/index2.js
@@ -7,8 +7,8 @@ const fetch = ({ page, entries }) => Promise.resolve(serverData.slice((page - 1)
 
 const rangePosition = ([b1, b2], [g1, g2]) => (
   (b1 >= g1 && b2 <= g2 && 'in') ||
-  (b1 >= g1 && b1 <= g2 && b2 >= g2 && 'right') ||
-  (b1 <= g1 && b2 >= g1 && b2 <= g2 && 'left') ||
+  (b1 >= g1 && b1 <= g2 + 1 && b2 >= g2 && 'right') ||
+  (b1 <= g1 && b2 >= g1 - 1 && b2 <= g2 && 'left') ||
   (b1 <= g1 && b2 >= g2 && 'over') ||
   ((b1 >= g2 || b2 <= g1) && 'out')
 )
@@ -38,5 +38,58 @@ const getOrFetch = ({ page, entries }) => {
       }, {})
 }
 
+const d1 = { '10-15': [1, 2, 3, 4, 5] }
+const d2 = { '25-35': [6, 7, 8, 9, 10] }
+
+const mergeKeys = (key1, value1, key2, value2) => {
+  const [from1, to1] = key1.split('-').map(v => parseInt(v, 10))
+  const [from2, to2] = key2.split('-').map(v => parseInt(v, 10))
+
+  switch (rangePosition([from1, to1], [from2, to2])) {
+    case 'in':
+    case 'right':
+    case 'left':
+    case 'over':
+      return {
+        [`${Math.min(from1, from2)}-${Math.max(to1, to2)}`]: [
+          ...value2.slice(0, Math.max(from1 - from2, 0)),
+          ...value1,
+          ...value2.slice((to1 - from2) + 1)
+        ]
+      }
+    case 'out':
+    default:
+      return { [key1]: value1, [key2]: value2 }
+  }
+}
+
+// console.log(mergeKeys(
+//   '35-40',
+//   [35, 36, 37, 38, 39, 40],
+//   '41-45',
+//   [41, 42, 43, 44, 45]
+// ))
+
+const merge = () => {
+  const d1Keys = Object.keys(d1)
+  const d2Keys = Object.keys(d2)
+
+  return _
+    .chain(d1)
+    .keys()
+    .reduce((acc, curr) => {
+      const [from, to] = curr.split('-')
+      return _
+        .chain(d2)
+        .keys()
+        .reduce((acc2, curr2) => {
+          const [from2, to2] = curr2.split('-')
+
+        }, {})
+        .value()
+    }, d2)
+    .value()
+}
+
 getOrFetch({ page: 7, entries: 5 })
-  .then(response => response)
+// console.log(merge())
